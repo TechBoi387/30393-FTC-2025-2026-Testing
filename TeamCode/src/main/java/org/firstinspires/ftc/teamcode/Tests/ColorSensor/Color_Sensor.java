60137a2b746e44563ebc3b2a5e699d44a626b516
Kevin <kevinbergel772@gmail.com>
2025-10-07 19:05:41 -0400

    With Test folder

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ColorSensor/Color_Sensor.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Tests.ColorSensor;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Color_Sensor extends OpMode {
+    Color_Sensor_Mechanics bench = new Color_Sensor_Mechanics();
+
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        Color_Sensor_Mechanics.DetectedColor detected = bench.getDetectedColor(telemetry);
+
+        String BallColor;
+        switch (detected) {
+            case PURPLEBALL: BallColor = "Purple"; break;
+            case GREENBALL: BallColor = "Green"; break;
+            default: BallColor = "Unknown";
+        }
+
+        telemetry.addData("Ball Color", BallColor);
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ColorSensor/Color_Sensor_Mechanics.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.Tests.ColorSensor;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Color_Sensor_Mechanics {
+    NormalizedColorSensor colorSensor;
+
+
+    public enum DetectedColor {
+        PURPLEBALL,
+        GREENBALL,
+        RED, // for testing
+        GREEN, //for testing
+        BLUE, //for testing
+        UNKNOWN
+    }
+
+    public void init(HardwareMap hwMap) {
+        colorSensor = hwMap.get(NormalizedColorSensor.class, "Color1");
+        colorSensor.setGain(15);
+    }
+
+
+    public DetectedColor getDetectedColor(Telemetry telemetry) {
+        NormalizedRGBA colors = colorSensor.getNormalizedColors(); // returns 4 values red, blue, green, alpha (brightness/ light)
+
+        float normRed, normGreen, normBlue;
+        normRed = colors.red / colors.alpha;
+        normGreen = colors.green / colors.alpha;
+        normBlue = colors.blue / colors.alpha;
+
+        telemetry.addData("red", normRed);
+        telemetry.addData("green", normGreen);
+        telemetry.addData("blue", normBlue);
+
+
+        /*
+        RED =1, .76, .54
+        GREEN = 0.47, 1, 0.74
+        YELLOW = 1, 1, 1
+        BLUE = .34, .75, 1
+        GREENBALL =
+        PURPLEBALL =
+         */
+
+        //TODO add if statements for specific colors added
+
+
+       /*  if (normRed > .85 && normGreen < .65 && normBlue < .64) {
+            return DetectedColor.RED;
+        } else if (normRed < .55 && normGreen > .85 && normBlue < .85) {
+            return DetectedColor.GREEN;
+        } else if (normRed < .45 && normGreen < .8 && normBlue > .85) {
+             return DetectedColor.BLUE;
+       */ if (normGreen > normBlue && normGreen > normRed && normRed < (normGreen / 2)) {
+                 return DetectedColor.GREENBALL;
+       } else if (normBlue > normGreen && normBlue > normRed) {
+                 return DetectedColor.PURPLEBALL;
+        } else return DetectedColor.UNKNOWN;
+    }
+}
